5260066b1255bf13a00a39ddd81c56dec9b48cce
Jerônimo Luiz <jeronimoluiz2011@live.com>
2022-04-10 19:47:19 -0300

    Merge

--- Backend/connection.py
+++ Backend/connection.py
@@ -1,13 +1,8 @@
 from pymongo import MongoClient
 import certifi
 
-#url = "localhost"
-#port = 27017
 
-uri = "mongodb+srv://admin-tcc:tcc2022@health-system-qrcode.nokkm.mongodb.net/Teste?retryWrites=true&w=majority"
 
 def connect_mongodb():
-    #client = MongoClient(url, port)
-    client = MongoClient(uri, tlsCAFile=certifi.where())
     db = client.Teste
     return db
